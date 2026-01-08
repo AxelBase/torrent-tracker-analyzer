ba0a692f32a3b1bea451c17e6433ba49231fa46f
Sahil Ramjeeth <22046468@dut4life.ac.za>
2026-01-08 12:43:36 +0200

    Fix B

---
+++ docs/_app/immutable/chunks/BjJkrfcU.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_pvjj6l?.base??"/torrent-tracker-analyzer",a=globalThis.__sveltekit_pvjj6l?.assets??s??"";export{a,s as b};
--- docs/_app/immutable/chunks/CIWxBq2X.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_7ysd69?.base??"/torrent-tracker-analyzer",a=globalThis.__sveltekit_7ysd69?.assets??s??"";export{a,s as b};
